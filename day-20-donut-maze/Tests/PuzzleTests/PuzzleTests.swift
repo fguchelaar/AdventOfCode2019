e18606e930798c097801d4998c127e2039cbb5fb
Frank Guchelaar <fguchelaar@gmail.com>
2019-12-22 21:13:14 +0100

    fixed paths to input

--- day-20-donut-maze/Tests/PuzzleTests/PuzzleTests.swift
+++ day-20-donut-maze/Tests/PuzzleTests/PuzzleTests.swift
@@ -4,17 +4,17 @@ import XCTest
 final class PuzzleTests: XCTestCase {
 
     func testPart1Example1()  {
-        let puzzle = Puzzle(input: try! String(contentsOfFile: "/Users/frank/Workspace/github/fguchelaar/AdventOfCode2019/day-20-donut-maze/test1.txt"))
+        let puzzle = Puzzle(input: try! String(contentsOfFile: "test1.txt"))
         XCTAssertEqual(puzzle.part1(), 23)
     }
 
     func testPart1Example2()  {
-        let puzzle = Puzzle(input: try! String(contentsOfFile: "/Users/frank/Workspace/github/fguchelaar/AdventOfCode2019/day-20-donut-maze/test2.txt"))
+        let puzzle = Puzzle(input: try! String(contentsOfFile: "test2.txt"))
         XCTAssertEqual(puzzle.part1(), 58)
     }
 
     func testPart2Example()  {
-        let puzzle = Puzzle(input: try! String(contentsOfFile: "/Users/frank/Workspace/github/fguchelaar/AdventOfCode2019/day-20-donut-maze/test3.txt"))
+        let puzzle = Puzzle(input: try! String(contentsOfFile: "test3.txt"))
         XCTAssertEqual(puzzle.part2(), 396)
     }
 
