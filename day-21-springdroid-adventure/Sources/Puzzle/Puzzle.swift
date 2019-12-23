e30055496ddf712e2b0882bb4834c7f032a424bc
Frank Guchelaar <fguchelaar@gmail.com>
2019-12-23 19:14:57 +0100

    completed part 1 of day 21

--- day-21-springdroid-adventure/Sources/Puzzle/Puzzle.swift
+++ day-21-springdroid-adventure/Sources/Puzzle/Puzzle.swift
@@ -10,16 +10,43 @@ import AdventKit
 
 public class Puzzle {
 
-    let input: String
+    let program: [Int]
     public init(input: String) {
-        self.input = input
+        program = input.array(separatedBy: CharacterSet(charactersIn: ","), using: Int.init)
     }
 
-    public func part1() -> String {
-        ""
+    public func part1() -> Int {
+        run(instructions: [
+            "NOT A J",
+            "NOT C T",
+            "AND D T",
+            "OR T J",
+            "WALK"
+        ])
     }
 
-    public func part2() -> String {
-        ""
+    public func part2() -> Int {
+        run(instructions: [
+            "RUN"
+        ], printOutput: true)
+    }
+
+    func run(instructions: [String], printOutput: Bool = false) -> Int {
+
+        let input = instructions
+            .flatMap { "\($0)\n" }
+            .map { Int($0.asciiValue!) }
+
+        let ic = IntCode(memory: program)
+        ic.runProgram(inputs: input)
+
+        if printOutput {
+            let map = String(ic.output.dropLast().map {
+                Character(UnicodeScalar($0)!)
+            })
+            print(map)
+        }
+
+        return ic.output.last!
     }
 }
