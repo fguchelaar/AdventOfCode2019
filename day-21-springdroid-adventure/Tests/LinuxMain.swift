3aca478749b7f2e8622d67f1fc4f34eb734232f2
Frank Guchelaar <fguchelaar@gmail.com>
2019-12-23 16:33:44 +0100

    fixed input path

--- day-21-springdroid-adventure/Tests/LinuxMain.swift
+++
@@ -1,7 +0,0 @@
-import XCTest
-
-import PuzzleTests
-
-var tests = [XCTestCaseEntry]()
-tests += PuzzleTests.allTests()
-XCTMain(tests)
--- day-22-slam-shuffle/Sources/Main/main.swift
+++ day-22-slam-shuffle/Sources/Main/main.swift
@@ -1,6 +1,6 @@
 import Puzzle
 
-let puzzle = Puzzle(input: try! String(contentsOfFile: "/Users/frank/Workspace/github/fguchelaar/AdventOfCode2019/day-22-slam-shuffle/input.txt"))
+let puzzle = Puzzle(input: try! String(contentsOfFile: "input.txt"))
 
 print("Part 1: \(puzzle.part1())")
 print("Part 2: \(puzzle.part2())")
