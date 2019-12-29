1c72d39b3396a26e4a3a0cefe91a592324e86e9e
Frank Guchelaar <fguchelaar@gmail.com>
2019-12-29 20:57:02 +0100

    started with day 18

--- day-18-many-worlds-interpretation/Sources/Puzzle/Puzzle.swift
+++ day-18-many-worlds-interpretation/Sources/Puzzle/Puzzle.swift
@@ -8,18 +8,92 @@
 import Foundation
 import AdventKit
 
+enum Tile {
+    case wall
+    case open
+    case key(letter: String.Element)
+    case door(letter: String.Element)
+    case entrance
+}
+
 public class Puzzle {
 
-    let input: String
+    var maze: [Point: Tile]
+    var keys: [Point: String.Element]
+    var doors: [Point: String.Element]
+    var entrance: Point
+
     public init(input: String) {
-        self.input = input
+        maze = [:]
+        keys = [:]
+        doors = [:]
+        entrance = .zero
+
+        input
+            .trimmingCharacters(in: .whitespacesAndNewlines)
+            .components(separatedBy: .newlines)
+            .enumerated().forEach { (y, line) in
+                line
+                    .enumerated().forEach { (x, character) in
+                        let point = Point(x: x, y: y)
+                        if character == "#" {
+                            maze[point] = .wall
+                        }
+                        else if character == "." {
+                            maze[point] = .open
+                        }
+                        else if character == "@" {
+                            maze[point] = .entrance
+                            entrance = point
+                        }
+                        else if character.isUppercase {
+                            maze[point] = .door(letter: character)
+                            doors[point] = character
+                        }
+                        else if character.isLowercase {
+                            maze[point] = .key(letter: character)
+                            keys[point] = character
+                        }
+                }
+        }
     }
 
-    public func part1() -> String {
-        ""
+    func astar(grid: [Point: Tile], from: Point, to: Point) -> [Point]? {
+
+        return nil
     }
 
-    public func part2() -> String {
-        ""
+    func draw(grid: [Point: Tile]) {
+        var output = ""
+        let maxPoint = grid.reduce(Point.min) { Point(x: max($0.x, $1.key.x), y: max($0.y, $1.key.y)) }
+        for y in 0...maxPoint.y {
+            for x in 0...maxPoint.x {
+                switch grid[Point(x: x, y: y)] {
+                case .wall:
+                    output.append("#")
+                case .open:
+                    output.append(".")
+                case .entrance:
+                    output.append("@")
+                case .door(let door):
+                    output.append(door)
+                case .key(let key):
+                    output.append(key)
+                default:
+                    output.append(" ")
+                }
+            }
+            output.append("\n")
+        }
+        print(output)
+    }
+
+    public func part1() -> Int {
+        draw(grid: maze)
+        return -1
+    }
+
+    public func part2() -> Int {
+        -1
     }
 }
--- day-18-many-worlds-interpretation/Tests/PuzzleTests/PuzzleTests.swift
+++ day-18-many-worlds-interpretation/Tests/PuzzleTests/PuzzleTests.swift
@@ -2,13 +2,70 @@ import XCTest
 @testable import Puzzle
 
 final class PuzzleTests: XCTestCase {
-
-    func testExample()  {
-        let puzzle = Puzzle(input: "")
-        XCTAssertEqual(puzzle.part1(), "")
+    
+    func testPart1Example1()  {
+        let puzzle = Puzzle(input: """
+        #########
+        #b.A.@.a#
+        #########
+        """ )
+        XCTAssertEqual(puzzle.part1(), 8)
     }
-
+    
+    func testPart1Example2()  {
+        let puzzle = Puzzle(input: """
+        ########################
+        #f.D.E.e.C.b.A.@.a.B.c.#
+        ######################.#
+        #d.....................#
+        ########################
+        """ )
+        XCTAssertEqual(puzzle.part1(), 86)
+    }
+    
+    func testPart1Example3()  {
+        let puzzle = Puzzle(input: """
+        ########################
+        #...............b.C.D.f#
+        #.######################
+        #.....@.a.B.c.d.A.e.F.g#
+        ########################
+        """ )
+        XCTAssertEqual(puzzle.part1(), 132)
+    }
+    
+    func testPart1Example4()  {
+        let puzzle = Puzzle(input: """
+        #################
+        #i.G..c...e..H.p#
+        ########.########
+        #j.A..b...f..D.o#
+        ########@########
+        #k.E..a...g..B.n#
+        ########.########
+        #l.F..d...h..C.m#
+        #################
+        """ )
+        XCTAssertEqual(puzzle.part1(), 136)
+    }
+    
+    func testPart1Example5()  {
+        let puzzle = Puzzle(input: """
+        ########################
+        #@..............ac.GI.b#
+        ###d#e#f################
+        ###A#B#C################
+        ###g#h#i################
+        ########################
+        """ )
+        XCTAssertEqual(puzzle.part1(), 81)
+    }
+    
     static var allTests = [
-        ("testExample", testExample),
+        ("testPart1Example1", testPart1Example1),
+        ("testPart1Example2", testPart1Example2),
+        ("testPart1Example3", testPart1Example3),
+        ("testPart1Example4", testPart1Example4),
+        ("testPart1Example5", testPart1Example5),
     ]
 }
