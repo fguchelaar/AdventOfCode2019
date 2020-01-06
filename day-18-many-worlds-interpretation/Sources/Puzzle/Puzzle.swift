8b485eb47814975c85f98e7ae54b06d1fac9ade8
Frank Guchelaar <fguchelaar@gmail.com>
2020-01-06 11:03:26 +0100

    implemented A* for finding the keys

--- day-18-many-worlds-interpretation/Sources/Puzzle/Puzzle.swift
+++ day-18-many-worlds-interpretation/Sources/Puzzle/Puzzle.swift
@@ -16,6 +16,19 @@ enum Tile {
     case entrance
 }
 
+extension Point {
+    var up: Point { Point(x: x, y: y-1) }
+    var down: Point { Point(x: x, y: y+1) }
+    var left: Point { Point(x: x-1, y: y) }
+    var right: Point { Point(x: x+1, y: y) }
+}
+
+extension Point: CustomStringConvertible {
+    public var description: String {
+        "(\(x),\(y))"
+    }
+}
+
 public class Puzzle {
 
     var maze: [Point: Tile]
@@ -58,8 +71,52 @@ public class Puzzle {
         }
     }
 
-    func astar(grid: [Point: Tile], from: Point, to: Point) -> [Point]? {
+    func astar(grid: [Point: Tile], start: Point, goal: Point, neighbors: (Point) -> [Point]) -> [Point]? {
 
+        func reconstruct(_ map: [Point: Point], _ point: Point) -> [Point] {
+            var current = point
+            var totalPath = [current]
+            while map.keys.contains(current) {
+                current = map[current]!
+                totalPath.insert(current, at: 0)
+            }
+            return totalPath
+        }
+
+        func h(_ point: Point) -> Int {
+            point.manhattan(to: goal)
+        }
+
+        var openSet = Set<Point>()
+        var cameFrom = [Point: Point]()
+        var gScore = [Point: Int]()
+        var fScore = [Point: Int]()
+
+        openSet.insert(start)
+        gScore[start] = 0
+        fScore[goal] = h(start)
+
+        var current: Point
+        while !openSet.isEmpty {
+            current = openSet.sorted { fScore[$0, default: Int.max] < fScore[$1, default: Int.max] }.first!
+            if current == goal {
+                return reconstruct(cameFrom, current)
+            }
+
+            openSet.remove(current)
+            for neighbor in neighbors(current) {
+                let tentativeScore = gScore[current, default: Int.max] + 1
+                if tentativeScore < gScore[neighbor, default: Int.max] {
+                    cameFrom[neighbor] = current
+                    gScore[neighbor] = tentativeScore
+                    fScore[neighbor] = gScore[neighbor, default: Int.max] + h(neighbor)
+                    if !openSet.contains(neighbor) {
+                        openSet.insert(neighbor)
+                    }
+                }
+            }
+
+        }
         return nil
     }
 
@@ -90,6 +147,37 @@ public class Puzzle {
 
     public func part1() -> Int {
         draw(grid: maze)
+
+        let neighbors: (Point) -> [Point] = { point in
+
+            // If we're standing on a key, only proceed if we have that key in
+            // our possesion. This way we can minimize visible paths to the
+            // 'direct' keys
+            if self.keys.keys.contains(point) {
+                return []
+            }
+
+            return [point.up, point.down, point.left, point.right].filter { p in
+                switch self.maze[p] {
+                case .open, .key:
+                    return true
+                case .door(let door):
+                    return false
+                default:
+                    return false
+                }
+            }
+        }
+
+        // Route to all keys
+        keys.sorted { $0.value < $1.value }.forEach {
+            if let route = astar(grid: maze, start: entrance, goal: $0.key, neighbors: neighbors) {
+                print ("\($0.value): \(route)")
+            } else {
+                print ("\($0.value): no route")
+            }
+        }
+
         return -1
     }
 
